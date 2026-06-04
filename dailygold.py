f42401932e4d06c62d4b17856889fa947a62d4d1
João Manoel <joaom.developer@gmail.com>
2026-06-03 22:47:57 -0300

    Código atualizado

--- dailygold.py
+++ dailygold.py
@@ -2,12 +2,19 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-code = "CTUBQSEUKSBHCB0QC"
+codes = {
+    "goldenCode": "D1XMLNVPKLCO7FSEU",
+    "telegramCode": "",
+    "xCode": "",
+    "metaCode": "X7K9P",
+    "discCode": "",
+    "faceitCode": ""
+}
 
 @app.route("/", methods=["GET"])
 def getCode():
-    return jsonify(code)
+    return jsonify(codes.goldenCode)
 
-print(code)
+print(codes)
 
 app.run(port=3000, host="0.0.0.0", debug=False)
